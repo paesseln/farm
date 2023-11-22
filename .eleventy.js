dfa47be0eab4b5d135f1a9ce0a38cd6ee0044390
Noah Paessel <paesseln@gcc.mass.edu>
2023-11-22 12:43:06 -0500

    Eleventy works

---
+++ .eleventy.js
@@ -0,0 +1,6 @@
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("styles/*");
+  eleventyConfig.addPassthroughCopy("images/*");
+  eleventyConfig.addPassthroughCopy("scripts/*");
+};
